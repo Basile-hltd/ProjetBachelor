4d97b3ac5193fea278664b9e9062b1d8a6791b4d
Hiltpold Basile (HES) <basile.hiltpold@hes-so.ch>
2025-05-27 18:29:51 +0200

    add first c file

---
+++ Software/dsPIC RF.X/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: basile.hiltpold
+ *
+ * Created on 27. mai 2025, 18:23
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    while(1){
+        
+    }
+}
+
